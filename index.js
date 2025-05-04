d97e7eeb43b6379548ce68c2003f48dad033ad60
Darwin Charris <hdarwin@uninorte.edu.co>
2025-05-04 14:05:31 -0500

    db

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express";
-
+import pg from "pg";
+import { config } from "dotenv";
+config();
 const app = express();
-
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 app.listen(3000);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  return res.json(result.rows[0]);
 });
 console.log("Server is running on port 3000");
